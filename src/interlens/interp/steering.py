29c81fd8ac57bf69c2f628b04590237b4d81e1f2
Siddharth M. Bhatia <siddharth@ai.stanford.edu>
2026-07-01 04:13:59 -0700

    Relicense AGPLv3-or-later -> AGPLv3-only; run tests on uv.lock changes
    - pyproject.toml + .licenserc.yaml: SPDX AGPL-3.0-only
    - drop 'any later version' clause from header template + all .py file headers
    - tests.yml: also trigger when uv.lock changes

--- src/interlens/interp/steering.py
+++ src/interlens/interp/steering.py
@@ -2,9 +2,8 @@
 # Copyright (C) 2026 Siddharth M. Bhatia
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of version 3 of the GNU Affero General Public License
+# as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
